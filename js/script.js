c85ceeaa8b0d79b975ad8c0b6226f966092e9137
Massimiliano <m.croci.ly@gmail.com>
2021-01-24 09:50:56 +0100

    finish off, Cristina's version

--- js/script.js
+++ js/script.js
@@ -105,14 +105,14 @@ $(document).ready(function() {
     family: 'fas',
   }
 ];
-  // array dei colori
+  // array dei colori che è stato creato per far coincidere gli index col array iconTypes
   const colors = [
     'blue', // per il type animal
     'orange', // per il type vegetable
     'purple', // per il type user
   ];
 
-  // filtriamo per estrarre il type animale, vegetale, utente
+  // ****** 1 step filtriamo per estrarre il type animale, vegetale, utente
   // se funziona trasformalo in una funzione e commentalo
   // const types = [];
   // icons.forEach((item) => {
@@ -121,19 +121,89 @@ $(document).ready(function() {
   //   }
   // });
   // console.log(types);
-  const iconTypes = getTypes(icons,type); // funzione che rappresenta il codice sopra di essa
+  const iconTypes = getTypes(icons,'type'); // funzione che rappresenta il codice sopra di essa
   console.log(iconTypes); // non lasciando log nelle funzioni, inseriamo il risultato della fun in una const per verificare col log se è corretto
+  // *********
+
+
+  // ****** 2 step
+  const iconsColor = icons.map((item) => {
+    // console.log(item.type);
+    const indexType = iconTypes.indexOf(item.type);
+    // console.log(indexType);
+    return{
+      ...item,
+      color : colors[indexType]
+    }
+  });
+  console.log(iconsColor);
+  // ********
+
+
+  // ********** 3 step print icons
+  const appendedIcons = $('.icons');
+  printIcons(iconsColor,appendedIcons);
+  const select = $('#type');
+  printOptions(iconTypes,select);
+  select.change(function() {
+    const selected =$(this).val();
+    console.log(selected);
+
+    const filteredIcons = filterValue(iconsColor,selected);
+    console.log(filteredIcons);
+
+    printIcons(filteredIcons,appendedIcons);
+  });
+
 
 });
 
 
-
-function getTypes(array,toFilter) {
+// area delle funzioni
+function getTypes(array,toFilter) { // l'argomento toFilter sarà un jolly per qualsiasi dato noi vogliamo dall'array...
   const types = [];
   array.forEach((item) => {
-    if(!types.includes(item.toFilter)){
-      types.push(item.toFilter);
+    if(!types.includes(item[toFilter])){ // ...bisogna metterlo tra quadre e tra '' quando si invoca la funzione
+      types.push(item[toFilter]);
     }
   });
-  return types
+  return types // il return sempre l'ultima azione da compiere, mettendolo sotto types.push ciclerebbe solo una volta
+}
+
+
+function printIcons(array,container) {
+  container.html(''); // lo svuotiamo ad ogni click della select option
+  array.forEach((item) => {
+    const {name,prefix,type,family,color} = item; // destrutturazione per facilitarci le cose con i backtick
+    container.append(
+      `
+        <div class="icon">
+          <i class="${family} ${prefix}${name}" style="color:${color}"></i>
+          <div class="title">${name.toUpperCase()}</div>
+        </div>
+      `
+    );
+  });
+}
+
+
+function printOptions(array,select) {
+  array.forEach((item) => {
+    select.append(
+      `
+        <option value="${item}">${item}</option>
+      `
+    );
+  });
+}
+
+
+function filterValue(array,type) {
+  const filteredIcons = array.filter((item) => {
+    return item.type == type;
+  });
+  if(filteredIcons.length > 0){
+    return filteredIcons;
+  }
+  return array;
 }
